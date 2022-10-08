9645b6be7a0da09032481254dcd05887d87725dc
Aden Collinge <aden@alveole.buzz>
2022-10-08 15:20:07 -0400

    tidy

--- src/App.js
+++ src/App.js
@@ -1,62 +1,36 @@
-import * as THREE from "three";
-import { Suspense, useRef, useState } from "react";
-import { Canvas, useFrame, useThree } from "@react-three/fiber";
-import {
-    Environment,
-    useScroll,
-    ScrollControls,
-    PresentationControls,
-    OrbitControls,
-    ContactShadows,
-    RandomizedLight,
-    AccumulativeShadows,
-} from "@react-three/drei";
+import { useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { Environment, OrbitControls, ContactShadows, SpotLight } from "@react-three/drei";
 import { useSpring, a } from "@react-spring/three";
 
-const colors = ["red", "gold", "teal"];
 const FRAME_SPACE = 0.15;
 
 const Frame = (props) => {
-    const ref = useRef();
-    // const scroll = useScroll();
-    // useFrame((state, delta) => {
-    //     const r2 = scroll.range(1 / 8, 1 / 4);
-    //     console.log(props.position);
-    //     ref.current.position.y = THREE.MathUtils.damp(
-    //         ref.current.position.y,
-    //         (Math.PI / 8) * r2 + props.position[1],
-    //         20 - props.position[2] * 10,
-    //         delta
-    //     );
-    // });
     const [hover, setHover] = useState();
-    const { scale } = useSpring({ scale: hover ? 1.5 : 1 });
-    const yFactor = (hover ? 1.2 : 1) + (props.hover ? 1.2 : 0);
-
+    const yFactor = (hover ? 1.4 : 1) + (props.hover ? 1.2 : 0);
     const { position } = useSpring({
+        delay: props.hover ? 200 : 300,
         position: [props.position[0], props.position[1] * yFactor, props.position[2]],
     });
-
     return (
         <a.group
-            scale={scale}
-            onPointerOver={() => {
+            onPointerOver={(e) => {
+                e.stopPropagation();
                 setHover(true);
+                props.setHover(true);
             }}
             onPointerOut={() => {
                 setHover(false);
             }}
-            ref={ref}
-            scale={0.2}
             {...props}
             position={position}
         >
-            <mesh castShadow position={[0, 1.5, 0]}>
-                <boxGeometry args={[6.5, 0.5, 0.5]} />
+            <mesh castShadow position={[0, 0.3, 0]}>
+                <boxGeometry args={[1.3, 0.1, 0.1]} />
                 <meshStandardMaterial color="lightblue" />
             </mesh>
             <mesh castShadow position={[0, 0, 0]}>
-                <boxGeometry args={[6, 3, 0.25]} />
+                <boxGeometry args={[1.2, 0.6, 0.05]} />
                 <meshStandardMaterial color="lightblue" />
             </mesh>
         </a.group>
@@ -64,25 +38,15 @@ const Frame = (props) => {
 };
 
 const Box = (props) => {
-    const ref = useRef();
-    // const scroll = useScroll();
-    // useFrame((state, delta) => {
-    //     const r1 = scroll.range(0 / 8, 1 / 4);
-    //     ref.current.position.y = THREE.MathUtils.damp(
-    //         ref.current.position.y,
-    //         (Math.PI / 3) * (r1 * props.position[1]) + props.position[1],
-    //         10,
-    //         delta
-    //     );
-    // });
     const { position } = useSpring({
+        delay: props.hover ? 0 : 500,
         position: props.hover
             ? [props.position[0], props.position[1] * 2, props.position[2]]
             : props.position,
     });
 
     return (
-        <a.group ref={ref} scale={1} position={position}>
+        <a.group scale={1} position={position}>
             <mesh castShadow position={[0.75, 0.5, 0.75]}>
                 <boxGeometry args={[0.1, 0.75, 1.8]} />
                 <meshStandardMaterial color="khaki" />
@@ -102,52 +66,37 @@ const Box = (props) => {
             {Array(11)
                 .fill()
                 .map((_, i) => (
-                    <Frame hover={props.hover} position={[0, 0.5, i * FRAME_SPACE]} />
+                    <Frame
+                        setHover={props.setHover}
+                        key={i}
+                        hover={props.hover}
+                        position={[0, 0.5, i * FRAME_SPACE]}
+                    />
                 ))}
         </a.group>
     );
 };
 const Base = (props) => {
-    const ref = useRef();
-    // const scroll = useScroll();
-    // useFrame((state, delta) => {
-    //     const r1 = scroll.range(0 / 4, 1 / 4);
-    //     ref.current.position.y = THREE.MathUtils.damp(
-    //         ref.current.position.y,
-    //         (Math.PI / 3) * (r1 * props.position[1]) + props.position[1],
-    //         10,
-    //         delta
-    //     );
-    // });
-    // const [hover, setHover] = useState();
     const { position } = useSpring({
+        delay: props.hover ? 0 : 500,
         position: props.hover
             ? [props.position[0], props.position[1] * 2, props.position[2]]
             : props.position,
     });
 
     return (
-        <a.mesh
-            // scale={scale}
-            ref={ref}
-            castShadow
-            // {...props}
-            position={position}
-        >
+        <a.mesh castShadow position={position}>
             <boxGeometry args={[1.75, 0.2, 2.2]} />
             <meshStandardMaterial color="gold" />
         </a.mesh>
     );
 };
 const Hive = (props) => {
-    const ref = useRef();
-    // const scroll = useScroll();
-    // useFrame((state, delta) => {
-    //     const r2 = scroll.range(0, 3 / 4);
-    //     ref.current.rotation.y = THREE.MathUtils.damp(ref.current.rotation.y, 3 * r2, 10, delta);
-    // });
     const [hover, setHover] = useState();
-    const { scale } = useSpring({ scale: hover ? 1.5 : 1 });
+    const { scale } = useSpring({
+        scale: hover ? props.scale * 1.5 : props.scale,
+        delay: hover ? 0 : 500,
+    });
 
     return (
         <a.group
@@ -159,12 +108,11 @@ const Hive = (props) => {
                 setHover(false);
             }}
             position={props.position}
-            ref={ref}
         >
             <Base hover={hover} position={[0, 2.6, 0.75]} />
-            <Box hover={hover} position={[0, 0.1, 0]} />
-            <Box hover={hover} position={[0, 0.9, 0]} />
-            <Box hover={hover} position={[0, 1.7, 0]} />
+            <Box hover={hover} setHover={setHover} position={[0, 0.1, 0]} />
+            <Box hover={hover} setHover={setHover} position={[0, 0.9, 0]} />
+            <Box hover={hover} setHover={setHover} position={[0, 1.7, 0]} />
             <Base hover={hover} position={[0, 0.1, 0.75]} />
         </a.group>
     );
@@ -172,27 +120,25 @@ const Hive = (props) => {
 function App() {
     return (
         <Canvas
-            style={{ width: "100%", height: "100%", position: "fixed", background: "skyblue" }}
+            style={{ width: "100%", height: "100%", position: "fixed" }}
             shadows
-            camera={{ position: [15, 15, 15], fov: 50 }}
+            camera={{ position: [10, 0, 10], fov: 80 }}
         >
-            {/* <ScrollControls
-                pages={1} // Each page takes 100% of the height of the canvas
-                distance={1} // A factor that increases scroll bar travel (default: 1)
-                damping={10} // Friction, higher is faster (default: 4)
-                horizontal={false} // Can also scroll horizontally (default: false)
-                infinite={false} // Can also scroll infinitely (default: false)
-            > */}
-          <ContactShadows opacity={.5} scale={10} blur={5} far={100} resolution={256} color="#000000" />
-            {/* <primitive object={new THREE.AxesHelper(10)} /> */}
+            <ContactShadows
+                opacity={0.5}
+                scale={10}
+                blur={5}
+                far={100}
+                resolution={256}
+                color="#000000"
+            />
             <OrbitControls
                 minPolarAngle={Math.PI / 3}
                 maxPolarAngle={Math.PI / 3}
                 enableZoom={false}
             />
-            <Hive position={[0, 1, -0.75]} />
+            <Hive scale={0.5} position={[0, 0.2, -0.75]} />
             <Environment preset="city" />
-            {/* </ScrollControls> */}
         </Canvas>
     );
 }
